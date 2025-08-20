675c46fbc8b08d0715bf5655d56d75f1bae56727
Simon H <github@pointbeing.net>
2025-08-20 09:38:03 +0100

    Add more model fields

--- clickthroughs/models.py
+++ clickthroughs/models.py
@@ -5,6 +5,9 @@ class Clickthrough(models.Model):
 
     clicked_at = models.DateTimeField(auto_now_add=True)
     ip_address = models.GenericIPAddressField(protocol='IPv4')
+    session_id = models.CharField(blank=True, null=True, max_length=64, verbose_name='Session ID')
+    user_agent = models.TextField(blank=True, null=True)
     url_to = models.URLField(blank=True, null=True, verbose_name='To')
-    path_from = models.CharField(blank=True, null=True, max_length=256, verbose_name='From')
+    hostname = models.CharField(blank=True, null=True, max_length=256)
+    path_from = models.TextField(blank=True, null=True, verbose_name='From')
 
