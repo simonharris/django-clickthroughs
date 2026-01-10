cdd39de4fd551b1d478a9eb9b8d4786f83d3feb5
Simon H <github@pointbeing.net>
2026-01-10 15:00:47 +0000

    Initial, basic  admin for affiliate campaigns

--- clickthroughs/admin.py
+++ clickthroughs/admin.py
@@ -2,6 +2,39 @@ from django.contrib import admin
 
 from .models import *
 
+
+## inlines --------------------------------------------------------------------
+
+
+class CampaignHostnameInline(admin.TabularInline):
+    model = CampaignHostname
+    extra = 1
+
+
+## admins ---------------------------------------------------------------------
+
+
+@admin.register(Platform)
+class PlatformAdmin(admin.ModelAdmin):
+
+    list_display = ['name']
+
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'slug', 'start_date', 'end_date']
+    prepopulated_fields = {'slug': ['name']}
+    inlines = [CampaignHostnameInline]
+
+
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+
+    list_display = ['platform', 'key', 'value']
+    list_filter = ['platform']
+
+
 @admin.register(Clickthrough)
 class ClickthroughAdmin(admin.ModelAdmin):
 
--- clickthroughs/models.py
+++ clickthroughs/models.py
@@ -1,6 +1,42 @@
 from django.db import models
 
 
+class Platform(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Parameter(models.Model):
+    platform = models.ForeignKey(Platform, on_delete=models.CASCADE, related_name='parameters')
+    key = models.CharField(max_length=64)
+    value = models.CharField(max_length=256)
+
+    class Meta:
+        unique_together = ('platform', 'key')  # ensures no duplicate keys per platform
+
+    def __str__(self):
+        return f"{self.key}={self.value} ({self.platform})"
+
+
+class Campaign(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(default='', null=False, unique=True)
+    platform = models.ForeignKey(Platform, on_delete=models.CASCADE, related_name='campaigns')
+    parameters = models.ManyToManyField(Parameter, blank=True, related_name='campaigns')
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class CampaignHostname(models.Model):
+    campaign = models.ForeignKey(Campaign, related_name='hostnames', on_delete=models.CASCADE)
+    hostname = models.CharField(max_length=255)
+
+
 class Clickthrough(models.Model):
 
     clicked_at = models.DateTimeField(auto_now_add=True)
