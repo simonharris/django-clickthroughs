b074175c1a0a3fb474b4b3e3646498cea266d02e
Simon H <github@pointbeing.net>
2026-01-10 23:45:29 +0000

    Bind campaign params into generated URL

--- clickthroughs/templatetags/click_tags.py
+++ clickthroughs/templatetags/click_tags.py
@@ -1,14 +1,53 @@
+from urllib.parse import urlparse, parse_qsl, urlencode
+
 from django import template
+from django.db.models import Q
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.http import urlencode
+
+from clickthroughs.models import CampaignHostname
 
 
 register = template.Library()
 
 @register.simple_tag
-def clickthrough(url: str) -> str:
+def clickthrough(next_url: str) -> str:
+    """Generate a clickthrough URL for the given target URL,
+    adding any campaign parameters as required.
+    """
+
+    # Determine campaign based on hostname
+    parsed_url = urlparse(next_url)
+    hostname = parsed_url.hostname
+
+    now = timezone.now().date()
+
+    ch = (
+        CampaignHostname.objects.select_related('campaign')
+        .filter(
+            hostname=hostname
+        ).filter(
+            Q(campaign__start_date__lte=now) | Q(campaign__start_date__isnull=True),
+            Q(campaign__end_date__gte=now) | Q(campaign__end_date__isnull=True),
+        )
+        .first()
+    )
+
+    campaign = ch.campaign if ch else None
+
+    if campaign is not None:
+        # Add campaign parameters to URL
+
+        params = dict(parse_qsl(parsed_url.query, keep_blank_values=True))
+
+        for p in campaign.parameters.all():
+            params[p.key] = p.value
+
+        new_query = urlencode(params, doseq=True)
+        next_url = parsed_url._replace(query=new_query).geturl()
+
 
     cturl = reverse('clickthroughs:go')
 
-    return cturl + '?' + urlencode({'to': url})
-
+    return cturl + '?' + urlencode({'to': next_url})
--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -6,9 +6,7 @@ class ClickthroughTemplateTagTests(TestCase):
     def test_url_tag(self):
         tpl = Template("{% load click_tags %}foo {% clickthrough 'https://example.com/some/page' %} bar")
         rendered = tpl.render(Context())
-        # TODO: use reverse here rather than hardcoding /go
-        self.assertIn('/go?to=https%3A%2F%2Fexample.com%2Fsome%2Fpage', rendered)
-
+        self.assertIn('/?to=https%3A%2F%2Fexample.com%2Fsome%2Fpage', rendered)
 
     # def test_affiliate_url_tag(self):
     #     tpl = Template('{% load click_tags %}{{ "https://example.com"|affiliate_url:"skiddle" }}')
