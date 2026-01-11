2c615524c18ab52a2dd194231c11f43826136918
Simon H <github@pointbeing.net>
2026-01-11 08:53:15 +0000

    Add tests for .clean(). Closes #4

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
 from clickthroughs.models import (
     Clickthrough,
@@ -9,6 +10,36 @@ from clickthroughs.models import (
 )
 
 class ClickthroughModelTests(TestCase):
+
+    def setUp(self):
+        self.platform = Platform.objects.create(name="Test Platform")
+
+        self.campaign1 = Campaign.objects.create(
+            name="Campaign 1",
+            slug="campaign-1",
+            platform=self.platform,
+            start_date="2024-01-01",
+            end_date="2024-12-31",
+        )
+        self.campaign2 = Campaign.objects.create(
+            name="Campaign 2",
+            slug="campaign-2",
+            platform=self.platform,
+            start_date="2024-06-01",
+            end_date="2024-12-31",
+        )
+
+        self.ch1 = CampaignHostname.objects.create(
+            campaign=self.campaign1,
+            hostname="overlap.com"
+        )
+
+        self.ch2 = CampaignHostname(
+            campaign=self.campaign2,
+            hostname="overlap.com"
+        )
+
+
     def test_create_clickthrough(self):
         c = Clickthrough.objects.create(
             ip_address='127.0.0.1',
@@ -17,6 +48,22 @@ class ClickthroughModelTests(TestCase):
         self.assertIsNotNone(c.clicked_at)
         self.assertEqual(c.ip_address, '127.0.0.1')
         self.assertEqual(c.url_to, 'https://example.com')
+
+
+    def test_campaign_hostname_overlap_prevention(self):
+
+        with self.assertRaises(ValidationError) as context:
+            self.ch2.clean()
+
+        self.assertIn(
+            "Hostname 'overlap.com' is already used by another active campaign.",
+            str(context.exception))
+
+
+    def test_campaign_hostname_overlap_with_self_is_ok(self):
+
+        self.ch2.pk = self.ch1.pk  # Simulate updating the same record
+        self.ch2.clean() # Should not raise any exception
 
 
 class ModelStrTests(TestCase):
